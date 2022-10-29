f6f0066e39c1b1ddf2cfd8700ca90cc7a6363318
alexis <alexis-ag@mail.ru>
2022-10-29 23:47:30 +0300

    fixing changes in states

--- Sources/Perdux/ActionDispatcher.swift
+++ Sources/Perdux/ActionDispatcher.swift
@@ -6,7 +6,43 @@ protocol ActionDispatcherSubscriber {
     func notify(_ action: PerduxAction) async
 }
 
-public typealias AD = ActionDispatcher
+extension Collection where Element == PerduxAction {
+    public func perform(
+            fileID: String = #fileID,
+            functionName: String = #function,
+            lineNumber: Int = #line
+    ) async {
+        await ActionDispatcher.emitAsync(self.map { $0 }, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+    }
+
+    public func delayedPerform(
+            delay: Double,
+            fileID: String = #fileID,
+            functionName: String = #function,
+            lineNumber: Int = #line
+    ) async {
+        await ActionDispatcher.emitAsync(self.map { $0 }, delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+    }
+}
+
+extension PerduxAction {
+    public func perform(
+            fileID: String = #fileID,
+            functionName: String = #function,
+            lineNumber: Int = #line
+    ) async {
+        await ActionDispatcher.emitAsync(self, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+    }
+
+    public func delayedPerform(
+            delay: Double,
+            fileID: String = #fileID,
+            functionName: String = #function,
+            lineNumber: Int = #line
+    ) async {
+        await ActionDispatcher.emitAsync(self, delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+    }
+}
 
 public class ActionDispatcher {
     private static var subscribers: [ActionDispatcherSubscriber] = []
