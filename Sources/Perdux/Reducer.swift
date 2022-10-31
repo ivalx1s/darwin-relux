569fcb2141378d41a6863a9ee3d45294596385ad
alexis <alexis-ag@mail.ru>
2022-10-31 22:13:12 +0300

    viewState, reducer

--- Sources/Perdux/Reducer.swift
+++ Sources/Perdux/Reducer.swift
@@ -1,8 +1,8 @@
 public struct Reducer<State: PerduxState, Action: PerduxAction> {
-    public let reduce: (State, Action) -> Void
+    public let reduce: (State, Action) async -> Void
 
     public init(
-            reduce: @escaping (State, Action) -> Void
+            reduce: @escaping (State, Action) async -> Void
     ) {
         self.reduce = reduce
     }
