8c8ef75d795fba65fdcf2e8886b9d417a703623b
alexis <alexis-ag@mail.ru>
2022-11-02 17:44:18 +0300

    supporting view states

--- Sources/Perdux/PerduxStore.swift
+++ Sources/Perdux/PerduxStore.swift
@@ -3,13 +3,18 @@ import Logger
 
 public class PerduxStore: ActionDispatcherSubscriber {
     private(set) var states: [PerduxState] = []
+    private(set) var viewStates: [any PerduxViewState] = []
 
     public init() {
         ActionDispatcher.connect(self)
     }
 
-    public func connect(state: PerduxState) {
+    public func connectState(state: PerduxState) {
         states.append(state)
+    }
+
+    public func connectViewState(state: any PerduxViewState) {
+        viewStates.append(state)
     }
 
     func notify(_ action: PerduxAction) async {
@@ -21,6 +26,11 @@ public class PerduxStore: ActionDispatcherSubscriber {
 
     public func getState<T: PerduxState>(_ type: T.Type) -> T {
         let state = states.first { $0 is T }
+        return state as! T
+    }
+
+    public func getViewState<T: PerduxViewState>(_ type: T.Type) -> T {
+        let state = viewStates.first { $0 is T }
         return state as! T
     }
 
