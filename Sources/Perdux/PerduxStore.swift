191b840e8f08827a42c64d6ec3712968157176b5
alexis <alexis-ag@mail.ru>
2022-11-26 12:14:00 +0300

    storing states in store by object identity

--- Sources/Perdux/PerduxStore.swift
+++ Sources/Perdux/PerduxStore.swift
@@ -1,36 +1,46 @@
 import Foundation
 import Logger
 
+extension PerduxState {
+    var key: ObjectIdentifier { .init(type(of: self)) }
+    static var key: ObjectIdentifier { .init(self) }
+}
+extension PerduxViewState {
+    var key: ObjectIdentifier { .init(type(of: self)) }
+    static var key: ObjectIdentifier { .init(self) }
+}
+
+
 public class PerduxStore: ActionDispatcherSubscriber {
-    private(set) var states: [PerduxState] = []
-    private(set) var viewStates: [any PerduxViewState] = []
+    private(set) var states: [ObjectIdentifier: any PerduxState] = [:]
+    private(set) var viewStates: [ObjectIdentifier: any PerduxViewState] = [:]
 
     public init() {
         ActionDispatcher.connect(self)
     }
 
-    public func connectState(state: PerduxState) {
-        states.append(state)
+    public func connectState(state: any PerduxState) {
+        states[state.key] = state
     }
 
     public func connectViewState(state: any PerduxViewState) {
-        viewStates.append(state)
+        viewStates[state.key] = state
     }
 
     func notify(_ action: PerduxAction) async {
         await states
-                .concurrentForEach { state in
-                    await state.reduce(with: action)
+                .concurrentForEach { pair in
+                    await pair.value.reduce(with: action)
                 }
     }
 
     public func getState<T: PerduxState>(_ type: T.Type) -> T {
-        let state = states.first { $0 is T }
+        let state = states[T.key]
         return state as! T
     }
 
     public func getViewState<T: PerduxViewState>(_ type: T.Type) -> T {
-        let state = viewStates.first { $0 is T }
+        let state = viewStates[T.key]
         return state as! T
     }
 
