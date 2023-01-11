c736bf5a7b448b09ea95af89ff16dd669bc8b28b
Ivan O <ioparin@pm.me>
2023-01-11 16:13:48 +0400

    changing single-action execution to resultBuilder to match multi-action execution

--- Sources/Perdux/ActionDispatcher/ActionDispatcher+ActionBuilder.swift
+++ Sources/Perdux/ActionDispatcher/ActionDispatcher+ActionBuilder.swift
@@ -2,6 +2,8 @@ import Foundation
 
 @resultBuilder
 public struct PerduxActionCompositionBuilder {
+	public static func buildBlock(_ action: PerduxAction) -> PerduxAction { action }
+	
     public static func buildBlock() -> [PerduxAction] { [] }
 
     public static func buildBlock(_ actions: PerduxAction...) -> [PerduxAction] {
--- Sources/Perdux/ActionDispatcher/ActionDispatcher+Interface.swift
+++ Sources/Perdux/ActionDispatcher/ActionDispatcher+Interface.swift
@@ -27,7 +27,7 @@ public func action(
 		fileID: String = #fileID,
 		functionName: String = #function,
 		lineNumber: Int = #line,
-		action: () -> PerduxAction,
+		@PerduxActionCompositionBuilder action: () -> PerduxAction,
 		label: (() -> String)? = nil
 ) async {
 	await _action(
@@ -54,6 +54,23 @@ public func performAsync(
 
 	Task(priority: taskPriority) {
 		await _actions(executionType, delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber, actions: actions, label: label)
+	}
+}
+
+@inlinable
+public func performAsync(
+	_ executionType: ActionDispatcher.ExecutionType = .serially,
+	withPriority taskPriority: TaskPriority? = nil,
+	delay: Seconds? = nil,
+	fileID: String = #fileID,
+	functionName: String = #function,
+	lineNumber: Int = #line,
+	@PerduxActionCompositionBuilder action: @escaping () -> PerduxAction,
+	label: (() -> String)? = nil
+) {
+	
+	Task(priority: taskPriority) {
+		await _action(executionType, delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber, action: action, label: label)
 	}
 }
 
@@ -86,18 +103,23 @@ internal func _actions(
 	}
 }
 
-
 @usableFromInline @inline(__always)
 internal func _action(
+	_ executionType: ActionDispatcher.ExecutionType = .serially,
 	delay: Seconds? = nil,
 	fileID: String = #fileID,
-    functionName: String = #function,
-    lineNumber: Int = #line,
-	action: () -> PerduxAction,
+	functionName: String = #function,
+	lineNumber: Int = #line,
+	@PerduxActionCompositionBuilder action: () -> PerduxAction,
 	label: (() -> String)? = nil
 ) async {
 	let execStartTime = timestamp.milliseconds
-	await ActionDispatcher.emitAsync(action(), delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+	switch executionType {
+		case .serially:
+			await ActionDispatcher.sequentialPerform([action()], delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+		case .concurrently:
+			await ActionDispatcher.concurrentPerform([action()], delay: delay, fileID: fileID, functionName: functionName, lineNumber: lineNumber)
+	}
 	let execDurationMillis = timestamp.milliseconds - execStartTime
 	
 	if let label {
