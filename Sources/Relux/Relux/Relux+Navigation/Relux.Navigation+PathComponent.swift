5505e6a164009b394b705862a9a78ff4e5a34bdf
Ivan Oparin <oparin@me.com>
2025-01-23 23:34:31 +0400

    Add modal router protocol

--- Sources/Relux/Relux/Relux+Navigation/Relux.Navigation+PathComponent.swift
+++ Sources/Relux/Relux/Relux+Navigation/Relux.Navigation+PathComponent.swift
@@ -1,3 +1,14 @@
 public extension Relux.Navigation {
 	protocol PathComponent: Hashable {}
 }
+
+
+public extension Relux.Navigation {
+    protocol ModalComponent: PathComponent, Identifiable {}
+}
+
+public extension Relux.Navigation.ModalComponent {
+    var id: Int {
+        self.hashValue
+    }
+}
--- Sources/Relux/Relux/Relux+Navigation/Relux.Navigation+RouterProtocol.swift
+++ Sources/Relux/Relux/Relux+Navigation/Relux.Navigation+RouterProtocol.swift
@@ -8,6 +8,16 @@ public extension Relux.Navigation {
     }
 }
 
+public extension Relux.Navigation {
+    protocol ModalRouterProtocol: RouterProtocol {
+        var maxDepth: Int { get }
+        
+        func reduce(with action: any Relux.Action) async
+        
+        func restore() async
+    }
+}
+
 internal extension Relux.Navigation.RouterProtocol {
 	nonisolated var key: ObjectIdentifier { .init(type(of: self)) }
 	nonisolated static var key: ObjectIdentifier { .init(self) }
